c51e96fede75f45884575ab3baff8988f27a4494
Jeremy Wilkins <jebw@bigmac.local>
2008-09-12 10:43:40 +0100

    Used script/generate plugin to create plugin properly, adds license and test framework

---
+++ tasks/sagefu_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :sagefu do
+#   # Task goes here
+# end
---
+++ test/sagefu_test.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+
+class SagefuTest < Test::Unit::TestCase
+  # Replace this with your real tests.
+  def test_this_plugin
+    flunk
+  end
+end
