7e69c24202c60293cc68518489eab302b0d24217
jebw <jeb@jdwilkins.co.uk>
2009-01-07 18:41:21 +0100

    beginnings of invoice support in DownloadHelper

--- lib/sage/download_helper.rb
+++ lib/sage/download_helper.rb
@@ -20,12 +20,52 @@ module Sage
         @xml, @view, @xml_options = xml, view, xml_options
       end
       
+      def customer(cust)
+        @xml.Customer do
+          @xml.CompanyName(cust.Company) if cust.methods.include?('Company')
+          @xml.CompanyName(cust.company) if cust.methods.include?('company')
+          @xml.CompanyName(cust.CompanyName) if cust.methods.include?('CompanyName')
+          @xml.CompanyName(cust.company_name) if cust.methods.include?('company_name')
+        end
+      end
+      
+      def invoices(invoices)
+        @xml.Invoices do
+          map = build_invoice_map(invoices.first) unless invoices.empty?
+        
+          invoices.each do |invoice|
+            @xml.Invoice do
+              map.each do |key, value|
+                @xml.__send__ key, invoice.send(value) unless value.nil?
+              end
+            end
+          end
+        end
+      end
+      
       private
         
         def method_missing(method, *arguments, &block)
           @xml.__send__(method, *arguments, &block)
         end
       
+        def build_invoice_map(invoice)
+          map = Hash.new
+          im = invoice.methods
+          
+          map['Id'] = first_match(im, ['Id', 'id'])
+          map['InvoiceNumber'] = first_match(im, ['InvoiceNumber', 'invoicenumber', 'invoice_number'])
+          
+          map
+        end
+        
+        def first_match(haystack, needles)
+          needles.each do |needle|
+            return needle if haystack.include?(needle)
+          end
+          
+          nil
+        end
     end
 
   end
